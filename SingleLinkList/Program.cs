d0739906aa80e0c101f7489932d931c79f789ed2
MJ24 <1093939518@qq.com>
2015-04-01 14:10:51 +0800

    单链表V1.0

--- SingleLinkList/Program.cs
+++ SingleLinkList/Program.cs
@@ -10,6 +10,24 @@ namespace SingleLinkList
     {
         static void Main(string[] args)
         {
+            SingleLinkList<int> list = new SingleLinkList<int>();
+            list.Append(1);
+            list.Append(2);
+            list.Reverse();
+            //list.Append(2);
+            list.Print();
+            //Console.WriteLine(list.LocateElem(3));
+            //list.InsertElem(1, 2342);
+            //list.Append(1);
+            //list.InsertElem(0, 2342);
+            //list.Print();
+            //list.InsertElem(1, 2);
+            //list.Print();
+            //list.InsertElem(2, 9);
+            //list.Print();
+            //list.InsertElem(77, 555);
+            //list.Print();
+            Console.ReadLine();
         }
     }
 }
--- SingleLinkList/SingleLinkList.cs
+++ SingleLinkList/SingleLinkList.cs
@@ -93,15 +93,140 @@ namespace SingleLinkList
             return result;
         }
 
+        //在第index个元素之前插入元素，因此当只有3个元素时不允许index=4
         public void InsertElem(int index, T elem)
         {
-            Node<T> node = new Node<T>(elem);
+            if (IsEmpty())
+            {
+                Console.WriteLine("链表为空！");
+            }
+            else if (index < 1)
+            {
+                Console.WriteLine("插入元素位置错误！");
+            }
+            else if (index == 1)
+            {
+                Node<T> node = new Node<T>(elem);
+                node.Next = head;
+                head = node;//插在第一个时记得改head指向
+            }
+            else
+            {
+                Node<T> afterIndexNode = head;
+                Node<T> beforeIndexNode = new Node<T>();
+                int realIndex = 1;
+                while (afterIndexNode.Next != null && realIndex < index)
+                {
+                    beforeIndexNode = afterIndexNode;
+                    afterIndexNode = afterIndexNode.Next;
+                    realIndex++;
+                }
+                if (realIndex == index)
+                {
+                    Node<T> node = new Node<T>(elem);
+                    beforeIndexNode.Next = node;
+                    node.Next = afterIndexNode;
+                }
+                else
+                {
+                    Console.WriteLine("插入元素位置错误！");
+                }
+            }
+        }
+
+        public T DeleteElem(int index)
+        {
+            T result = default(T);
+            if (IsEmpty())
+            {
+                Console.WriteLine("链表为空！");
+            }
+            else if (index < 1)
+            {
+                Console.WriteLine("删除元素位置错误！");
+            }
+            else if (index == 1)
+            {
+                result = head.Data;
+                head = head.Next;
+            }
+            else
+            {
+                Node<T> afterIndexNode = head;
+                Node<T> beforeIndexNode = new Node<T>();
+                int realIndex = 1;
+                while (afterIndexNode.Next != null && realIndex < index)
+                {
+                    beforeIndexNode = afterIndexNode;
+                    afterIndexNode = afterIndexNode.Next;
+                    realIndex++;
+                }
+                if (realIndex == index)
+                {
+                    result = afterIndexNode.Data;
+                    beforeIndexNode.Next = afterIndexNode.Next;
+                }
+                else
+                {
+                    Console.WriteLine("删除元素位置错误！");
+                }
+            }
+            return result;
+        }
+
+        public int LocateElem(T value)
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("链表为空！");
+                return -1;
+            }
+            int index = 1;
             Node<T> p = head;
-            int realIndex = 0;
-            while (p.Next != null && realIndex < index)
+            while (p != null && !p.Data.Equals(value))
             {
                 p = p.Next;
-                realIndex++;
+                index++;
+                if (p == null)
+                {
+                    Console.WriteLine("链表中不存在此元素！");
+                    index = -1;
+                }
+            }
+            return index;
+        }
+
+        //空链表、单节点链表也满足
+        public void Reverse()
+        {
+            Node<T> p = head;
+            Node<T> q = new Node<T>();
+            Node<T> R = new Node<T>();
+            while (p != null)
+            {
+                q = p;
+                p = p.Next;
+                q.Next = R.Next;
+                R.Next = q;
+            }
+            head = R.Next;
+            R = null;
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("链表为空！");
+            }
+            else
+            {
+                Node<T> p = head;
+                while (p != null)
+                {
+                    Console.WriteLine(p.Data);
+                    p = p.Next;
+                }
             }
         }
     }
